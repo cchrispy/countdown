0e2c8426aec82ffa67a949a9097b1c18d0252fcc
Chris Chiang <cchiang1992@gmail.com>
2017-03-18 17:57:43 -0700

    Component set up

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Main from './components/Main.jsx';
+
+render(
+  <Main />,
+  document.getElementById('main')
+)
